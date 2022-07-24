0de6f36eb15f8a2a84a44cac984485d7940d8585
tabuyos <tabuyos@outlook.com>
2022-07-25 00:45:21 +0800

    add template file

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("VERSION_CATALOGS")
+
+// explicitly specify composite build project by include
+includeBuild("build-logic")
+
+rootProject.name = "iris-template"
+
+include("app", "list", "utilities")
